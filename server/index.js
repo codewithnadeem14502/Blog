a82cd13d446d8d822297a56fc265bc0695db93ba
codewithnadeem14502 <codewithnadeem@14502>
2024-03-24 06:25:46 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.HOST_URL],
     // origin: ["https://quillify-iota.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders:
@@ -60,5 +60,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to my blog server!");
 });
 app.listen(PORT, () => {
+  // console.log(process.env.HOST_URL);
   console.log("Server is working");
 });
